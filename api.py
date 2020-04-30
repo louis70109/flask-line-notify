eb0c8e722beb02a9c38c569310a994c2825b7109
NiJia <louis70109@gmail.com>
2020-04-30 16:11:29 +0800

    增加 lotify 支援的所有功能

--- api.py
+++ api.py
@@ -1,49 +1,84 @@
-import requests
+import uuid
 from flask import Flask, render_template, request
 import os
 
+from lotify.client import Client
+
 app = Flask(__name__)
 
-CLIENT_ID = os.getenv('CLIENT_ID')
-SECRET = os.getenv('CLIENT_SECRET')
-URI = os.getenv('REDIRECT_URI')
+CLIENT_ID = os.getenv('LINE_CLIENT_ID')
+SECRET = os.getenv('LINE_CLIENT_SECRET')
+URI = os.getenv('LINE_REDIRECT_URI')
+lotify = Client(client_id=CLIENT_ID, client_secret=SECRET, redirect_uri=URI)
+
+
+# or use by default
+# lotify = Client()
 
 
 @app.route('/')
 def home():
-    return render_template('notify_index.html', CLIENT_ID=CLIENT_ID, URI=URI)
+    link = lotify.get_auth_link(state=uuid.uuid4())
+    return render_template('notify_index.html', auth_url=link)
 
 
-@app.route('/notify/check')
+@app.route('/callback')
 def confirm():
-    client = {
-        'grant_type': 'authorization_code',
-        'code': request.args.get('code'),
-        'redirect_uri': URI,
-        'client_id': CLIENT_ID,
-        'client_secret': SECRET
-    }
-    r = requests.post(
-        'https://notify-bot.line.me/oauth/token', data=client)
-    if r.status_code == 200:
-        payload = r.json()
-        return render_template('notify_confirm.html', token=payload.get('access_token'))
-    return {'message': 'Error'}, 400
+    token = lotify.get_access_token(code=request.args.get('code'))
+    return render_template('notify_confirm.html', token=token)
 
 
 @app.route('/notify/send', methods=['POST'])
 def send():
     payload = request.get_json()
-    r = requests.post(
-        "https://notify-api.line.me/api/notify",
-        headers={
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': payload.get('token')
-        },
-        data={'message': payload.get('message')}
+    response = lotify.send_message(
+        access_token=payload.get('token'),
+        message=payload.get('message')
     )
-    return {'result': r.text()}, r.status_code
+    return {'result': response.get('message')}, response.get('status')
+
+
+@app.route('/notify/send/sticker', methods=['POST'])
+def send_sticker():
+    payload = request.get_json()
+    response = lotify.send_message_with_sticker(
+        access_token=payload.get('token'),
+        message=payload.get('message'),
+        sticker_id=630,
+        sticker_package_id=4
+    )
+    return {'result': response.get('message')}, response.get('status')
+
+
+@app.route('/notify/send/url', methods=['POST'])
+def send_url():
+    payload = request.get_json()
+    response = lotify.send_message_with_image_url(
+        access_token=payload.get('token'),
+        message=payload.get('message'),
+        image_fullsize=payload.get('url'),
+        image_thumbnail=payload.get('url')
+    )
+    return {'result': response.get('message')}, response.get('status')
+
+
+@app.route('/notify/send/path', methods=['POST'])
+def send_file():
+    payload = request.get_json()
+    response = lotify.send_message_with_image_path(
+        access_token=payload.get('token'),
+        message=payload.get('message'),
+        image_path='./test_data/dog.png'
+    )
+    return {'result': response.get('message')}, response.get('status')
+
+
+@app.route('/notify/revoke', methods=['POST'])
+def revoke():
+    payload = request.get_json()
+    response = lotify.revoke(access_token=payload.get('token'))
+    return {'result': response.get('message')}, response.get('status')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
